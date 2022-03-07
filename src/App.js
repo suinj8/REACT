5a1904aabdc5810bde8d67bf8d1da63696807e08
suinj8 <suin8@jbnu.ac.kr>
2022-03-08 00:17:03 +0900

    삭제버튼 구현, local storage를 이용하여 일기를 저장하고 불러오기

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useReducer, useRef } from "react";
+import React, { useEffect, useReducer, useRef } from "react";
 
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
@@ -31,49 +31,30 @@ const reducer = (state, action) => {
     default:
       return state;
   }
+
+  localStorage.setItem("diary", JSON.stringify(newState));
   return newState;
 };
 
 export const DiaryStateContext = React.createContext();
 export const DiaryDispatchContext = React.createContext();
 
-const dummyData = [
-  {
-    id: 1,
-    emotion: 1,
-    content: "일기 1번",
-    date: 1646305755039,
-  },
-  {
-    id: 2,
-    emotion: 2,
-    content: "일기 2번",
-    date: 1646305755040,
-  },
-  {
-    id: 3,
-    emotion: 3,
-    content: "일기 3번",
-    date: 1646305755041,
-  },
-  {
-    id: 4,
-    emotion: 4,
-    content: "일기 4번",
-    date: 1646305755042,
-  },
-  {
-    id: 5,
-    emotion: 5,
-    content: "일기 5번",
-    date: 1646305755043,
-  },
-];
-
 function App() {
-  const [data, dispatch] = useReducer(reducer, dummyData);
+  const [data, dispatch] = useReducer(reducer, []);
 
-  const dataId = useRef(6);
+  useEffect(() => {
+    const localData = localStorage.getItem("diary");
+    if (localData) {
+      const diaryList = JSON.parse(localData).sort(
+        (a, b) => parseInt(b.id) - parseInt(a.id)
+      );
+      dataId.current = parseInt(diaryList[0].id) + 1;
+
+      dispatch({ type: "INIT", data: diaryList });
+    }
+  }, []);
+
+  const dataId = useRef(0);
   // Create
   const onCreate = (date, content, emotion) => {
     dispatch({
